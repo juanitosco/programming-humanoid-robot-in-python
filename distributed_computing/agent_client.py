d4affbe740394976a98d81d8588e6945b4ff5a4a
juanitosco <jascovino99@gmail.com>
2023-12-03 18:26:53 +0100

    homework

--- distributed_computing/agent_client.py
+++ distributed_computing/agent_client.py
@@ -7,6 +7,8 @@
 '''
 
 import weakref
+from threading import Thread
+from jsonrpc import jsonrpcclient
 
 class PostHandler(object):
     '''the post hander wraps function to be excuted in paralle
@@ -17,11 +19,16 @@ class PostHandler(object):
     def execute_keyframes(self, keyframes):
         '''non-blocking call of ClientAgent.execute_keyframes'''
         # YOUR CODE HERE
+        process = Thread(target=self.proxy.execute_keyframes, args=[keyframes])
+        process.start()
 
     def set_transform(self, effector_name, transform):
         '''non-blocking call of ClientAgent.set_transform'''
         # YOUR CODE HERE
+        process = Thread(target=self.proxy.set_transform, args=[effector_name, transform])
+        process.start()
 
+    
 
 class ClientAgent(object):
     '''ClientAgent request RPC service from remote server
@@ -29,35 +36,37 @@ class ClientAgent(object):
     # YOUR CODE HERE
     def __init__(self):
         self.post = PostHandler(self)
-    
+
     def get_angle(self, joint_name):
-        '''get sensor value of given joint'''
-        # YOUR CODE HERE
-    
+        '''Get sensor value of given joint'''
+        response = jsonrpcclient.request("http://localhost:9999", "get_angle", joint_name)
+        return response.result
+
     def set_angle(self, joint_name, angle):
-        '''set target angle of joint for PID controller
-        '''
-        # YOUR CODE HERE
+        '''Set target angle of joint for PID controller'''
+        response = jsonrpcclient.request("http://localhost:9999", "set_angle", joint_name, angle)
+        return response.result
 
     def get_posture(self):
-        '''return current posture of robot'''
-        # YOUR CODE HERE
+        '''Return current posture of robot'''
+        response = jsonrpcclient.request("http://localhost:9999", "get_posture")
+        return response.result
 
     def execute_keyframes(self, keyframes):
-        '''excute keyframes, note this function is blocking call,
-        e.g. return until keyframes are executed
-        '''
-        # YOUR CODE HERE
+        '''Execute keyframes, blocking call'''
+        response = jsonrpcclient.request("http://localhost:9999", "execute_keyframes", keyframes)
+        return response.result
 
     def get_transform(self, name):
-        '''get transform with given name
-        '''
-        # YOUR CODE HERE
+        '''Get transform with given name'''
+        response = jsonrpcclient.request("http://localhost:9999", "get_transform", name)
+        return response.result
 
     def set_transform(self, effector_name, transform):
-        '''solve the inverse kinematics and control joints use the results
-        '''
-        # YOUR CODE HERE
+        '''Solve the inverse kinematics and control joints using the results'''
+        response = jsonrpcclient.request("http://localhost:9999", "set_transform", effector_name, transform)
+        return response.result
+
 
 if __name__ == '__main__':
     agent = ClientAgent()
